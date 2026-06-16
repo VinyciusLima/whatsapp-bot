439dcdd9d3d120cc74085e0cf669871618f8feb9
VinyciusLima <arquivodovi2@gmail.com>
2026-06-16 01:00:59 -0300

    chore: ajusta porta para 8080

--- agendador.js
+++ agendador.js
@@ -20,7 +20,7 @@ app.get('/', async (req, res) => {
   `);
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`🌐 Servidor rodando na porta ${PORT}`));
 
 // ─── Cliente WhatsApp ───────────────────────────────────────────────────────
